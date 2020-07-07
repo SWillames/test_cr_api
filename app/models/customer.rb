509d1ccbcfb9914b1a2fab5a1ce48f5988ab7b46
SWillames <sergiowoc@gmail.com>
2020-07-07 16:04:35 -0300

    2 functionality: each word of the name with a capital letter.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :carts
+
+  def name
+  	b = []
+    self[:name].split.each do |x|
+    	b << x.capitalize
+    end
+
+    b.join(" ")
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Customer, type: :model do
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
+
+  context '#name' do
+    let(:cust) { Customer.new(name: 'sergio willames') }
+
+    it 'titleize name' do
+      expect(cust.name).to eq('Sergio Willames')
+    end
+  end
 end
