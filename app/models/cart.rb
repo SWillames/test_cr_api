e691f3bf842da75d1486d0eeda08c80684036bce
SWillames <sergiowoc@gmail.com>
2020-07-08 04:57:51 -0300

    3 features: total calculation of the cart price.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :customer
   has_many :cart_items
+
+  def total_cart_price
+    total_price = 0
+    CartItem.all.map do |x|
+      medicine = Medicine.find(x.medicine_id)
+      total_price = x.quantity * medicine.value
+    end
+
+    total_price
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Cart, type: :model do
   context 'relationships' do
     it { is_expected.to belong_to :customer }
   end
+
+  context 'total cart price' do
+    before(:each) do
+      Customer.new(name: 'customer test').save
+      Cart.new(customer_id: Customer.last.id).save
+      Medicine.new(name: 'Piroxcan', value: 8.0, quantity: 30, stock: 30).save
+    end
+
+    it 'calculates total cart' do
+      c = CartItem.new(cart_id: Cart.last.id, medicine_id: Medicine.last.id, quantity: 20).save
+      
+      
+      expect(Cart.last.total_cart_price).to eql 160.0
+    end
+  end
 end
