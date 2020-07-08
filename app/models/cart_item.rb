78f7a602434c5a9ed0d7a4035bf5d412c92b76ad
SWillames <sergiowoc@gmail.com>
2020-07-08 05:00:05 -0300

    4) functionalities: adding an item to the cart and removing it from the medicine stock.

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,29 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :medicine
+
+  validates :cart_id, :medicine_id, :quantity, presence: true
+  validate :positived_stock, :quantity_positived
+  after_save :remove_from_stock
+  
+  private
+
+  def positived_stock
+    med = Medicine.find(medicine_id)
+
+    errors.add(:quantity, 'No stock of medicine') unless med.stock.positive?
+  end
+
+  def quantity_positived
+    med = Medicine.find(medicine_id)
+
+    errors.add(:quantity, 'insufficient quantity') unless (med.stock - quantity).positive?
+  end
+
+  def remove_from_stock
+    med = Medicine.last
+    cart = CartItem.last
+    med.stock -= cart.quantity
+    med.save!
+  end
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe CartItem, type: :model do
     it { is_expected.to belong_to :cart }
     it { is_expected.to belong_to :medicine }
   end
+
+  context '#adding to cart' do
+    before(:each) do
+      Customer.new(name: 'customer test').save
+      Cart.new(customer_id: Customer.last.id).save
+      Medicine.new(name: 'Piroxcan', value: 7.0, quantity: 30, stock: 30).save
+    end
+
+    it 'insufficient stock' do
+      expect(CartItem.count).to eql 0
+      CartItem.new(cart_id: Cart.last.id, medicine_id: Medicine.last.id, quantity: 31).save
+      expect(CartItem.count).to eql 0
+    end
+
+    it 'remove from stock' do
+      CartItem.new(cart_id: Cart.last.id, medicine_id: Medicine.last.id, quantity: 10).save
+      expect(Medicine.last.stock).to eql 20
+    end
+  end
 end
